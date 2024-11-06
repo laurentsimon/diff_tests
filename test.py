7edf0660eb86cb70c813f4dd9f91c0014041b586
Laurent Simon <laurentsimon@google.com>
2024-11-06 17:51:39 +0000

    typo

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 
 
-def numerial_derivative1(f, x, epsilon=1e-5):
+def numerical_derivative1(f, x, epsilon=1e-5):
     delta_f = f(x+epsilon) - f(x)
     return delta_f / epsilon
 
-def numerial_derivative2(f, x, epsilon=1e-5):
-    f1_xpe = numerial_derivative1(f, x+epsilon, epsilon)
-    f1_x = numerial_derivative1(f, x, epsilon)
+def numerical_derivative2(f, x, epsilon=1e-5):
+    f1_xpe = numerical_derivative1(f, x+epsilon, epsilon)
+    f1_x = numerical_derivative1(f, x, epsilon)
     return (f1_xpe - f1_x) / epsilon
 
 x_points = [1, 10, 50, 75, 100]
@@ -16,8 +16,8 @@ for x in x_points:
     f_1 = lambda x:10*(x**9)
     f_2 = lambda x:90*(x**8)
     
-    v_1 = numerial_derivative1(f, x)
-    v_2 = numerial_derivative2(f, x)
+    v_1 = numerical_derivative1(f, x)
+    v_2 = numerical_derivative2(f, x)
 
     print()
     print(f_1(x), v_1)
